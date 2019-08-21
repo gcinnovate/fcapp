e0f449aa3d90f081739fcdac0b9e17d0886a966c
Samuel Sekiwere <sekiskylink@gmail.com>
2019-08-21 23:31:06 +0300

    add missing handler

---
+++ www/app/controllers/locations_handler.py
@@ -0,0 +1,25 @@
+import web
+import json
+# import pprint
+from . import db, regionDistricts, districtSubcounties
+from app.tools.utils import get_webhook_msg_old
+# from settings import config
+
+
+class SubRegionDistricts:
+    def POST(self, region):
+        # params = web.input()
+        web.header("Content-Type", "application/json; charset=utf-8")
+        if region in regionDistricts:
+            return json.dumps(regionDistricts[region])
+
+        return json.dumps({})
+
+
+class DistrictSubcounties:
+    def POST(self, district):
+        # params = web.input()
+        web.header("Content-Type", "application/json; charset=utf-8")
+        if district in districtSubcounties:
+            return json.dumps(districtSubcounties[district])
+        return json.dumps({})
