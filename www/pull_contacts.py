391d75458841f007cb9a899daab85d1fda6e01a1
sekiskylink <sekiskylink@gmail.com>
2020-05-09 16:11:34 +0300

    enable use of file of contact uuids to import contacts in fcapp_flow_data table

--- www/pull_contacts.py
+++ www/pull_contacts.py
@@ -9,11 +9,14 @@ import datetime
 import getopt
 
 cmd = sys.argv[1:]
-opts, args = getopt.getopt(cmd, 'g:', [])
+opts, args = getopt.getopt(cmd, 'g:f:', [])
 group = ''
+filename = ''
 for option, parameter in opts:
     if option == '-g':
         group = parameter
+    if option == '-f':
+        filename = parameter
 
 conn = psycopg2.connect(
     "dbname=" + config["db_name"] + " host= " + config["db_host"] + " port=" + config["db_port"] +
@@ -62,61 +65,66 @@ if group:
     contactsUrl += "group={}".format(group)
 print(contactsUrl)
 
-response = get_request(contactsUrl)
-contacts = response.json()
-for contact in contacts["results"]:
-    request_args = {}
-    values = {}
+with open(filename, 'r') as f:
+    for l in f:
+        uuid = l.strip()
+        if '&' not in contactsUrl:
+            contactsUrl += '&uuid={}'.format(uuid)
+        response = get_request(contactsUrl)
+        contacts = response.json()
+        for contact in contacts["results"]:
+            request_args = {}
+            values = {}
 
-    values["name"] = contact["name"]
-    values["created_on"] = contact["created_on"]
-    values["modified_on"] = contact["modified_on"]
+            values["name"] = contact["name"]
+            values["created_on"] = contact["created_on"]
+            values["modified_on"] = contact["modified_on"]
 
-    request_args["contact_uuid"] = contact["uuid"]
-    request_args["report_type"] = "contacts"
-    request_args["msisdn"] = ''
-    request_args["call_from_script"] = True
+            request_args["contact_uuid"] = contact["uuid"]
+            request_args["report_type"] = "contacts"
+            request_args["msisdn"] = ''
+            request_args["call_from_script"] = True
 
-    if (len(contact["urns"]) > 0):
-        request_args["msisdn"] = contact["urns"][0].replace('tel:', '')
+            if (len(contact["urns"]) > 0):
+                request_args["msisdn"] = contact["urns"][0].replace('tel:', '')
 
-    for key, val in contact["fields"].items():
-        if key in requestArgsFields:
-            request_args[key] = '{}'.format(val) if val else ''
-        if key in requiredFields:
-            values[key] = '{}'.format(val) if val else ''
-    # print(values)
-    # print(request_args)
-    print("======>", request_args)
-    # requests.post(FLOW_DATA_API, params=request_args, data=json.dumps(values))
+            for key, val in contact["fields"].items():
+                if key in requestArgsFields:
+                    request_args[key] = '{}'.format(val) if val else ''
+                if key in requiredFields:
+                    values[key] = '{}'.format(val) if val else ''
+            # print(values)
+            # print(request_args)
+            print("======>", request_args)
+            # requests.post(FLOW_DATA_API, params=request_args, data=json.dumps(values))
 
-    district_id = allDistrictsByName.get(request_args.get('district'), None)
-    district_operater = '=' if district_id else 'IS'
-    SQL = ("SELECT id FROM fcapp_flow_data WHERE district %s " % district_operater)
-    SQL += (
-        " %s AND facilityuid = %s AND msisdn=%s "
-        "AND report_type = %s AND contact_uuid = %s"
-    )
-    cur.execute(SQL, [
-        district_id, request_args.get('facilityuid'),
-        request_args.get('msisdn'), 'contacts', request_args.get('contact_uuid')])
-    res = cur.fetchone()
-    if res:
-        rpt_id = res['id']
-        cur.execute(
-            "UPDATE fcapp_flow_data SET values=%s, updated= NOW() WHERE id = %s", [
-                psycopg2.extras.Json(values, dumps=simplejson.dumps), rpt_id])
-    else:
-        cur.execute(
-            "INSERT INTO fcapp_flow_data (msisdn, contact_uuid, district, facility, facilityuid, subcounty, parish,"
-            "village, report_type, values, year, month) "
-            "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s::jsonb, %s, %s)", [
-                request_args['msisdn'], request_args['contact_uuid'],
-                district_id, request_args['facility'], request_args['facilityuid'],
-                request_args['sub_county'], request_args['parish'], request_args['village'],
-                'contacts', psycopg2.extras.Json(values, dumps=simplejson.dumps),
-                year, month
-            ]
-        )
-        conn.commit()
+            district_id = allDistrictsByName.get(request_args.get('district'), None)
+            district_operater = '=' if district_id else 'IS'
+            SQL = ("SELECT id FROM fcapp_flow_data WHERE district %s " % district_operater)
+            SQL += (
+                " %s AND facilityuid = %s AND msisdn=%s "
+                "AND report_type = %s AND contact_uuid = %s"
+            )
+            cur.execute(SQL, [
+                district_id, request_args.get('facilityuid'),
+                request_args.get('msisdn'), 'contacts', request_args.get('contact_uuid')])
+            res = cur.fetchone()
+            if res:
+                rpt_id = res['id']
+                cur.execute(
+                    "UPDATE fcapp_flow_data SET values=%s, updated= NOW() WHERE id = %s", [
+                        psycopg2.extras.Json(values, dumps=simplejson.dumps), rpt_id])
+            else:
+                cur.execute(
+                    "INSERT INTO fcapp_flow_data (msisdn, contact_uuid, district, facility, facilityuid, subcounty, parish,"
+                    "village, report_type, values, year, month) "
+                    "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s::jsonb, %s, %s)", [
+                        request_args['msisdn'], request_args['contact_uuid'],
+                        district_id, request_args['facility'], request_args['facilityuid'],
+                        request_args['sub_county'], request_args['parish'], request_args['village'],
+                        'contacts', psycopg2.extras.Json(values, dumps=simplejson.dumps),
+                        year, month
+                    ]
+                )
+                conn.commit()
 conn.close()
